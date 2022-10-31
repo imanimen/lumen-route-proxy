9c85c8b4fad23adbe2332ede4d8fbd1bfcc112cc
Iman Imen <imanimen81@gmail.com>
2022-10-31 10:21:11 +0330

    ADD check class

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class BaseController extends Controller
 {
     public function route(Request $request, $action){
+        // call the action
+        $class_action = base_path() . '../../../Actions/' . ucfirst($action) .'Action.php';
+        if (!$class_action)
+        {
+            return null;
+        }
         return $action;
     }
 }
