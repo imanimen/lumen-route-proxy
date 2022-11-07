0f79261a39b5722edf6ea86e766355e20bf4e53b
Iman Imen <imanimen81@gmail.com>
2022-11-07 16:46:12 +0330

    FIX docs, idk how but it works now

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,11 +9,11 @@ class Controller extends BaseController
     //
         /**
      * @OA\Info(
-     *   title="Example API",
+     *   title="Lumen Framework",
      *   version="1.0",
      *   @OA\Contact(
-     *     email="support@example.com",
-     *     name="Support Team"
+     *     email="imanimen81@gmail.com",
+     *     name="iman"
      *   )
      * )
      */
--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class BaseController extends Controller
+class DocsController extends Controller
 {
     /**
      * @OA\Post(
