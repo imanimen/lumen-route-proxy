12b1676aecebb2a5ec207319b2617573ef5fe319
Iman Imen <imanimen81@gmail.com>
2022-11-07 16:52:07 +0330

    ADD started writing command to genereate docs

---
+++ app/Console/Commands/CreateDocsCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+
+
+class CreateDocsCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = "gen:doc";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "generate api documentation";
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      try {
+        // check the actions
+
+        // check methods
+
+        //code...
+
+        // put_file_contents() in DocsController
+      } catch (\Throwable $th) {
+        //throw $th;
+        $this->error("Faild to genereate docs!");
+      }
+    }
+}
