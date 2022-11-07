6a82f0ae433a929b52787b7432c2aa7e39409517
Iman Imen <imanimen81@gmail.com>
2022-11-07 15:47:26 +0330

    ADD responseFactory to base controller

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -12,34 +12,49 @@ class BaseController extends Controller
         $class_action = 'App\\Actions\\'.ucfirst($action).'Action';
         if (!class_exists( $class_action ))
         {
-            return $this->fail([], 'not found', 404);
+            return $this->responseFacotry([], 'not found', [], 404);
         }
         $class = (new $class_action());
         if ($class->method() !== $request->method())
         {
-            return $this->fail([],'invalid method',$code=405);
+            return $this->responseFacotry([], 'invalid method', [], 405);
         }
         $validaton = Validator::make($request->all(), $class->validation());
         if ($validaton->fails())
         {
-            return $this->fail([], $validaton->errors(), $validaton->errors()->first(), 422);
+            return $this->responseFacotry([], $validaton->errors(), $this->validationMessages($validaton->messages()), 422);
         }
-        return $this->success([$class->render()]);
+        return $this->responseFacotry($class->render());
     }
 
 
+    public function validationMessages( $errors )
+    {
+        $messages = [];
+
+        foreach ( $errors as $error )
+        {
+            foreach ( $error as $message )
+            {
+                $messages[] = $message;
+            }
+        }
+
+        return $messages;
+
+    }
 
     public function success($data=[], $message=[], $errors=[], $code=200)
     {
-        return $this->whiteHouse($data, $message, $errors, $code);
+        return $this->responseFacotry($data, $message, $errors, $code);
     }
 
     public function fail($data = [], $message=[], $errors=[], $code=422)
     {
-        return $this->whiteHouse($data, $message, $errors, $code);
+        return $this->responseFacotry($data, $message, $errors, $code);
     }
 
-    public function whiteHouse($data=[], $errors=[], $message=[],int $code = 200)
+    public function responseFacotry($data=[], $errors=[], $message=[],int $code = 200)
     {
         return response()->make([
             'data' => $data,
